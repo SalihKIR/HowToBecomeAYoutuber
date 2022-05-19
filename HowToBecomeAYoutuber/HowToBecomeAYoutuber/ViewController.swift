e841033bd0f5dcd4d612fd90ad17261f95210246
Salih KIR <salihkir2020@gmail.com>
2022-05-19 14:55:11 +0300

    Rename project

--- HowToBecomeAYoutuber/HowToBecomeAYoutuber/ViewController.swift
+++ HowToBecomeAYoutuber/HowToBecomeAYoutuber/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Firstflow: UIViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Scenes/ExplanationFlow/ExplanationFlow.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ExplanationFlow.swift
-//  HowToBecomeAYoutuber
-//
-//  Created by Salih KIR on 19.05.2022.
-//
-
-import Foundation
