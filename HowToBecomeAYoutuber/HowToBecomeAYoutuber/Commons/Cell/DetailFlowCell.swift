82568ba82bf147e8d1f7ca308f61482913b2e050
Salih KIR <salihkir2020@gmail.com>
2022-05-19 19:37:01 +0300

    Update

---
+++ HowToBecomeAYoutuber/HowToBecomeAYoutuber/Commons/Cell/DetailFlowCell.swift
@@ -0,0 +1,23 @@
+//
+//  DetailFlowCell.swift
+//  HowToBecomeAYoutuber
+//
+//  Created by Salih KIR on 19.05.2022.
+//
+
+import UIKit
+
+class DetailFlowCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
